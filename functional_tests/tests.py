9b499c4e8005c92d9671030dde2e352737dc10c7
sitrus <tomaszlaszczuk@gmail.com>
2015-07-23 15:58:36 +0200

    Using Live server test case in functional tests. Functional test is now a module

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-import unittest
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.browser.implicitly_wait(3)
 
         # She notices the page title and header mention to-do lists
@@ -54,6 +54,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
         self.fail('Finish tests')
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
