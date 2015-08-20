e54d32f36e9a8272cf065c2d3e1324ded7efb068
t.laszczuk <t.laszczuk@futuremind.com>
2015-08-20 08:52:33 +0200

    Created requirements. Deleted admin from urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+# from django.contrib import admin
 
 from lists import views as list_views
 from lists import urls as list_urls
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
 ]
